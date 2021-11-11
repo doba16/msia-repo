b2b65980ffc3ac81dbac0d05b70ce02f0fa2a695
= <=>
2021-11-11 10:08:25 +0100

    Added a new method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        start();
+    }
+
+    public static void start() {
+        System.out.println("I am a new method!");
     }
 }
