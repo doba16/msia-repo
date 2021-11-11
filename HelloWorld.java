b2f31c2ed8a8ea8cefbb248f830c57b952633beb
= <=>
2021-11-11 10:26:59 +0100

    Added method sageHallo()

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,14 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         start();
+        sageHallo();
     }
 
     public static void start() {
         System.out.println("I am a new method!");
+    }
+
+    public static void sageHallo() {
+        System.out.println("Hallo!");
     }
 }
